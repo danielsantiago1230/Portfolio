1e582663bf068570aeeb941253ce5c07b1c98b31
Santiago Quintero <danielsantiago1230@gmail.com>
2022-04-22 13:26:40 -0300

    SHOWING THE ARTICLES FROM THE MENU

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,90 @@
 const interface= ()=>{
     const articlePointer=document.querySelectorAll(".contentCards");
     const menuPointer=document.querySelectorAll(".navButton");
-    menuPointer.forEach(Element=>(
-        Element.addEventListener("click",()=>{
-            console.log("hola");
+    
+    /*default For showing only ront End article*/
+    const defaultNavButomSelect=menuPointer[0].classList.contains("navButomSelect");
+        if(defaultNavButomSelect==true){
+            articlePointer.forEach(article=>{
+                if(article!=articlePointer[0]){
+                    article.style.display="none"
+                }
+            })
+        }
+    /* -----------ends defaut */
+    
+    /*event click in the menu articles */
+    menuPointer.forEach(menuhandleClick=>(
+        menuhandleClick.addEventListener("click",()=>{
+            articlePointer.forEach(articleHandleClick=>{
+                if(menuPointer[1]==menuhandleClick && articleHandleClick!=articlePointer[1] ){
+                        articleHandleClick.style.display="none";
+                        articlePointer[1].style.display="";
+                        /* changes in the buttom styles*/
+                        menuPointer.forEach(changeStyleButtom=>{
+                            if(menuPointer[1]==changeStyleButtom){
+                                changeStyleButtom.classList.add("navButomSelect");
+                                changeStyleButtom.classList.remove("navButomUnselect")
+                            }
+                            else if(menuPointer[1]!=changeStyleButtom && changeStyleButtom.classList!="navButomUnselect"){
+                                changeStyleButtom.classList.remove("navButomSelect")
+                                changeStyleButtom.classList.add("navButomUnselect")
+                            }
+                        })
+                        /*ends */
+                }
+                else if(menuPointer[2]==menuhandleClick && articleHandleClick!=articlePointer[2]){
+                    articleHandleClick.style.display="none";
+                    articlePointer[2].style.display="";
+                    /* changes in the buttom styles*/
+                    menuPointer.forEach(changeStyleButtom=>{
+                        if(menuPointer[2]==changeStyleButtom){
+                            changeStyleButtom.classList.add("navButomSelect");
+                            changeStyleButtom.classList.remove("navButomUnselect")
+                        }
+                        else if(menuPointer[2]!=changeStyleButtom && changeStyleButtom.classList!="navButomUnselect"){
+                            changeStyleButtom.classList.remove("navButomSelect")
+                            changeStyleButtom.classList.add("navButomUnselect")
+                        }
+                    })
+                    /*ends */
+                }
+                else if(menuPointer[3]==menuhandleClick && articleHandleClick!=articlePointer[3])
+                {
+                    articleHandleClick.style.display="none";
+                    articlePointer[3].style.display="";
+                    /* changes in the buttom styles*/
+                    menuPointer.forEach(changeStyleButtom=>{
+                        if(menuPointer[3]==changeStyleButtom){
+                            changeStyleButtom.classList.add("navButomSelect");
+                            changeStyleButtom.classList.remove("navButomUnselect")
+                        }
+                        else if(menuPointer[3]!=changeStyleButtom && changeStyleButtom.classList!="navButomUnselect"){
+                            changeStyleButtom.classList.remove("navButomSelect")
+                            changeStyleButtom.classList.add("navButomUnselect")
+                        }
+                    })
+                    /*ends */
+                }
+                else if(menuPointer[0]==menuhandleClick && articleHandleClick!=articlePointer[0]){
+                    articleHandleClick.style.display="none";
+                    articlePointer[0].style.display="";
+                    /* changes in the buttom styles*/
+                    menuPointer.forEach(changeStyleButtom=>{
+                        if(menuPointer[0]==changeStyleButtom){
+                            changeStyleButtom.classList.add("navButomSelect");
+                            changeStyleButtom.classList.remove("navButomUnselect")
+                        }
+                        else if(menuPointer[0]!=changeStyleButtom && changeStyleButtom.classList!="navButomUnselect"){
+                            changeStyleButtom.classList.remove("navButomSelect")
+                            changeStyleButtom.classList.add("navButomUnselect")
+                        }
+                    })
+                    /*ends */
+                    
+                }
+            })
+            
         })
     ))
 
